3539ba5ea033d889eb8205504f797cb25e8379ad
Diego Moura <59176835+diegomouradev@users.noreply.github.com>
2021-10-16 12:55:32 -0400

    static homapage done

---
+++ frontend/src/app/components/preview-panel/preview-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preview-panel',
+  templateUrl: './preview-panel.component.html',
+  styleUrls: ['./preview-panel.component.css']
+})
+export class PreviewPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

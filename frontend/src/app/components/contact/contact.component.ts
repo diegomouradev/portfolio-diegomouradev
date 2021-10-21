927ffd3781d9b8b529a0e265d0d025b8215476d6
Diego Moura <59176835+diegomouradev@users.noreply.github.com>
2021-10-21 08:36:42 -0400

    google analytics

--- frontend/src/app/components/contact/contact.component.ts
+++ frontend/src/app/components/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactForm } from 'src/app/models/contact-form';
 import { EmailService } from 'src/app/services/email.service';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -8,11 +9,40 @@ import { EmailService } from 'src/app/services/email.service';
 })
 export class ContactComponent implements OnInit {
   form = { name: '', email: '', message: '' };
+  // private MJ_APIKEY_PUBLIC = '60c16b936e5494f9d1c6c5c93609b627';
+  // private MJ_APIKEY_PRIVATE = 'e7c201dcd09adf143a128ca8ad2a62a8';
   constructor(private emailService: EmailService) {}
 
   ngOnInit(): void {}
 
   onFormSubmit($event): void {
-    console.log($event);
+    // mailjet.connect(this.MJ_APIKEY_PUBLIC, this.MJ_APIKEY_PRIVATE);
+    // const eventString = JSON.stringify($event);
+    // const request = mailjet.post('send', { version: 'v3.1' }).request({
+    //   Messages: [
+    //     {
+    //       From: {
+    //         Email: 'diegoo.demoura@gmail.com',
+    //         Name: 'Me',
+    //       },
+    //       To: [
+    //         {
+    //           Email: 'diegoo.demoura@gmail.com',
+    //           Name: 'You',
+    //         },
+    //       ],
+    //       Subject: 'test 1',
+    //       TextPart: 'Greetings from Mailjet!',
+    //       HTMLPart: `${eventString}`,
+    //     },
+    //   ],
+    // });
+    // request
+    //   .then((result) => {
+    //     console.log(result.body);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err.statusCode);
+    //   });
   }
 }

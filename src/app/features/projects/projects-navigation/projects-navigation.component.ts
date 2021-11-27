4eaec3e58259558cf3fb190243c9fb94bdc3c0e4
Diego Dev <diegodev@Diegos-MacBook-Air.local>
2021-11-27 11:49:22 -0500

    currentRoute prop added

--- src/app/features/projects/projects-navigation/projects-navigation.component.ts
+++ src/app/features/projects/projects-navigation/projects-navigation.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-projects-navigation',
   templateUrl: './projects-navigation.component.html',
-  styleUrls: ['./projects-navigation.component.scss']
+  styleUrls: ['./projects-navigation.component.scss'],
 })
 export class ProjectsNavigationComponent implements OnInit {
+  currentRoute: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.currentRoute = this.route.snapshot.data.url;
   }
-
 }
